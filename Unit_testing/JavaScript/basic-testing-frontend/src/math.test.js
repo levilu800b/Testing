971cbe90e7e6e5c7bb5fedd68b759a96e6852fc0
levilu800b <levilu800b@gmail.com>
2023-12-19 08:48:53 +0000

    Behavior and code error fix

--- Unit_testing/JavaScript/basic-testing-frontend/src/math.test.js
+++ Unit_testing/JavaScript/basic-testing-frontend/src/math.test.js
@@ -16,3 +16,23 @@ it('Should summarize all number values in an array', () => {
     );
     expect(result).toBe(expectedResult);
 });
+
+it('Should yield NaN if at least one invalid number is provided', () => {
+    const input = ['invalid', 1];
+
+    const result = add(input);
+
+    expect(result).toBeNaN();
+});
+
+it('Should yield a correct sum if an array of numeric string values is provided', () => {
+    const input = ['1', '2']
+
+    const result = add(input);
+
+    const expectedResult = input.reduce(
+        (prevValue, currValue) => +prevValue + +currValue,
+        0
+    );
+    expect(result).toBe(expectedResult);
+});
