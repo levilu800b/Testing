f079f5abc0af166c2aaea220d17fd63972abe9b5
levilu800b <levilu800b@gmail.com>
2023-12-19 09:39:29 +0000

    problem and solution

---
+++ Unit_testing/JavaScript/basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,19 @@
+import { it, expect } from 'vitest';
+
+import { transformToNumber } from './numbers';
+
+it('Should transform a string number to a number of type number', () => {
+    const input = '1';
+
+    const result = transformToNumber(input);
+
+    expect(result).toBeTypeOf('number');
+})
+
+it('Should yield NaN for non-transformable values', () => {
+    const input = 'invalid';
+
+    const result = transformToNumber(input);
+
+    expect(result).toBeNaN();
+})
