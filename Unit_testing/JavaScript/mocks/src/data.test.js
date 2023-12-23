4683bed64a2b144428a50107f29d2d1b0988cfff
levilu800b <levilu800b@gmail.com>
2023-12-23 23:11:05 +0000

    mocking functionalities

--- Unit_testing/JavaScript/mocks/src/data.test.js
+++ Unit_testing/JavaScript/mocks/src/data.test.js
@@ -6,6 +6,8 @@ describe('generateReportData()', () => {
   it('should execute logFn if provided', () => {
     const logger = vi.fn();
 
+    // logger.mockImplementationOnce(() => {});
+
     generateReportData(logger);
 
     expect(logger).toBeCalled();
